3bd41c7e87fa5551e4b16b5fc41bfcc3e1591aea
Samunosuke <mke1@gmx.at>
2017-12-08 16:33:57 +0100

    V 0.2 - added bottom colored line for the issues rows - removed gradient from the issue rows - did not look nice

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ Thanks Tim Petruski!
 
 [Download](https://github.com/Samunosuke/mantis-suke-theme/zipball/master)
 
-Installation:
+## Version
+0.2
+
+## Installation:
 1. Copy the MantisThemeManager folder to /mantis/plugins (.../mantis/plugins/MantisThemeManager)
 2. Create the themes folder in the mantis/css directory if it does not yet exist
 3. Copy the suke folder with all content into the mantis themes folder (.../mantis/css/themes/suke)
@@ -19,7 +22,7 @@ Installation:
 6. Open the MantisThemeManager configuration interface (click on the name which became a link)
 7. Choose the theme and apply by clicking the "Change theme" button
 
-Tested with Mantis BT 2.3.0
+Tested with Mantis BT 2.3.0 & 2.9.0
 
 ## ToDo
 - change pure java script implementation to use jQuery
@@ -28,3 +31,12 @@ Tested with Mantis BT 2.3.0
 ## I know
 - it does not yet look nice / it is just a proof of concept
 - it's a hackish implementation
+
+Changes:
+V 0.2 
+- added bottom colored line for the issues rows
+- removed gradient from the issue rows - did not look nice 
+
+V 0.1
+first proof of concept
+
--- suke/style.js
+++ suke/style.js
@@ -1,34 +1,43 @@
+/* V 0.2 by Samunosuke */
 window.addEventListener('load', overrideStyles, false);
 
 function overrideStyles(){
 	try {
 
-        /* Set brand name to be white again */
+      /* Set brand name to be white again - this is neccessary because color overrides in the default.css */
     	var navbarBrand = document.getElementsByClassName('navbar-brand');
     	navbarBrand[0].firstChild.style.color = '#FFFFFF';
     
-    	/*  */
+    	/* Add gradient to my buglist entries - uses the status colors of the mantis installation or set by custom values */
     	var bgColor;
     	var rows = document.getElementsByClassName('my-buglist-bug');
     	for(i=0; i<rows.length; i++) {
         	var elem = rows[i].getElementsByClassName('fa');
             var cStyle = window.getComputedStyle(elem[0]);
         	bgColor = cStyle.backgroundColor;
-        	rows[i].style.backgroundImage = '-moz-linear-gradient(to left, ' + bgColor + ', white)';
+        	rows[i].style.backgroundImage = '-moz-linear-gradient(to top, #DEDEDE, white)';
+            rows[i].style.height = '55px';
+        	rows[i].style.borderBottom = "thick solid "
+            rows[i].style.borderBottomColor = bgColor;
     	}
     
-		var bgColor;
-		var buglist = document.getElementById('buglist');
-		var bugListChild = buglist.getElementsByTagName('tbody');
-		var rows = bugListChild[0].getElementsByTagName('tr');
-		for (i = 0; i < rows.length; i++) {
-  			var colStatus = rows[i].getElementsByClassName('column-status');
-  			var elem = colStatus[0].getElementsByClassName('align-left');
-  			var iElement = elem[0].getElementsByClassName('fa-square');
-        	var cStyle = window.getComputedStyle(iElement[0]);
-  			bgColor = cStyle.backgroundColor;
-  			rows[i].style.backgroundImage = '-moz-linear-gradient(to left, ' + bgColor + ', white, white, white, white, '+ bgColor +')';
+    	/* Improve style for all buglist entries */
+			var bgColor;
+			var buglist = document.getElementById('buglist');
+			var bugListChild = buglist.getElementsByTagName('tbody');
+			var rows = bugListChild[0].getElementsByTagName('tr');
+			for (i = 0; i < rows.length; i++) {
+					var colStatus = rows[i].getElementsByClassName('column-status');
+					var elem = colStatus[0].getElementsByClassName('align-left');
+					var iElement = elem[0].getElementsByClassName('fa-square');
+		      var cStyle = window.getComputedStyle(iElement[0]);
+					bgColor = cStyle.backgroundColor;
+					/* add some nicer looking gradient */
+		      rows[i].style.backgroundImage = '-moz-linear-gradient(to top, #CECECE, white)';
+		      rows[i].style.height = '55px';
+		      /* add a bottom colored line to the issue row */
+		      rows[i].style.borderBottom = "thick solid "
+		      rows[i].style.borderBottomColor = bgColor;
 		}
 	}catch(err){ console.log(err); }
 	window.removeEventListener('load', overrideStyles);
-}
